4024002016624ff16d85d9e767f903ad46e4ac05
Joel Cano A <joel.Alarcon1@gmail.com>
2018-05-10 09:08:40 -0500

    Se agrega header json

---
+++ config/cors.php
@@ -0,0 +1,19 @@
+<?php 
+
+return [
+     /*
+     |--------------------------------------------------------------------------
+     | Laravel CORS
+     |--------------------------------------------------------------------------
+     |
+     | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+     | to accept any value.
+     |
+     */
+    'supportsCredentials' => false,
+    'allowedOrigins' => ['https://contabilizate12.herokuapp.com/'],
+    'allowedHeaders' => ['Content-Type', 'application/json'],
+    'allowedMethods' => ['*'], // ex: ['GET', 'POST', 'PUT',  'DELETE']
+    'exposedHeaders' => [],
+    'maxAge' => 0,
+];
